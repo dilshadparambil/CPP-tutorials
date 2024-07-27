9911a06a0a14a1b96502581be2f8900de3340312
Dilshad P <dilshadkareemparambil@gmail.com>
2024-07-27 14:56:39 +0530

    Create 6)vector_exaple.cpp
    program to find the sum of even numbers and the product of odd numbers in a vector.
    
    For example:
    
    Suppose we have a vector that is {2, 4, 3, 6, 1, 9}.
    
    Then the program should output:
    
    Sum of even numbers is 12
    Product of odd numbers is 27

---
+++ 6)vector_exaple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+int main(){
+
+  int sum=0,prod=1;
+  std::vector<int> numbers = {2,4,3,6,1,9};
+  for(int i=0;i<numbers.size();i++)
+  {
+    if(numbers[i]%2==0){
+      sum=sum+numbers[i];
+    }
+    else{
+      prod=prod*numbers[i];
+    }
+  }
+  std::cout<<"Sum of even numbers is "<<sum<<"\n";
+  std::cout<<"Product of odd numbers is "<<prod<<"\n";
+}
